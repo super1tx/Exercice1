13e2599a61debdcd5b40dd96a815feea1d79a2a1
Tristan <Tristanml_88@hotmail.com>
2023-11-08 18:02:35 -0500

    1234

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import click
 
+__version__ = "0.1.0"
+
+
+@click.command("version")
+def version():
+    click.echo(__version__)
+
+
 @click.command("hello")
-
 def hello():
- click.echo("Hello, World!")
-
-
+    click.echo("Hello, World!")
 
 
 if __name__ == "__main__":
- hello()
+    hello()
